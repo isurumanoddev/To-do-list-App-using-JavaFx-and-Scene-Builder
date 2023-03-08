d6c16982e08b33251622839849553addfa4361b5
isurumanoddev <isurumanod99@gmail.com>
2023-03-08 18:10:32 +0530

    add btnOnActionLogin method

--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -16,5 +16,54 @@ import java.sql.*;
 
 
 public class LoginFormController {
+    public AnchorPane root;
+    public TextField txtUserName;
+    public TextField txtPassword;
+
+    public static  String userid;
+    public static String username;
+
+
+    public void btnOnActionLogin(ActionEvent actionEvent) throws IOException {
+
+        String username = txtUserName.getText();
+        String password = txtPassword.getText();
+
+        Connection connection = DBconnection.getInstance().getConnection();
+        try {
+            PreparedStatement preparedStatement =connection.prepareStatement("select * from user where user_name=? and password=?");
+            preparedStatement.setObject(1,username);
+            preparedStatement.setObject(2,password);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            boolean isExist = resultSet.next();
+
+            if(isExist){
+                userid=resultSet.getString(1);
+                username=resultSet.getString(2);
+
+                Parent parent = FXMLLoader.load(getClass().getResource("../view/ToDoListForm.fxml"));
+                Scene scene=new Scene(parent);
+
+                Stage stage= (Stage) this.root.getScene().getWindow();
+                stage.setScene(scene);
+                stage.setTitle("to do list");
+                stage.centerOnScreen();
+
+            }
+            else{
+                new Alert(Alert.AlertType.ERROR,"invalid username or password").showAndWait();
+
+                txtPassword.clear();
+                txtUserName.clear();
+                txtUserName.requestFocus();
+            }
+
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+    }
 
 }
