48dd8330762905bcfedd3a32faf572d1bc837a8b
isurumanoddev <isurumanod99@gmail.com>
2023-03-08 18:04:34 +0530

    add controllers

---
+++ src/controller/CreateUserFormController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import db.DBconnection;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.*;
+public class CreateUserFormController {
+}
